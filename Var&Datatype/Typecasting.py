c845d1ab7aff95418b86716b02a1417c6ed8ead9
yousuf-safin <yousufalisafin@gmail.com>
2025-05-12 02:02:33 +0600

    TypeCasting

---
+++ Var&Datatype/Typecasting.py
@@ -0,0 +1,14 @@
+a=1 #integer
+b=2.2 #float
+c= "Yousuf" #string
+d= True #boolean
+e= None #null
+f= "22.22"
+g= float(f)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
