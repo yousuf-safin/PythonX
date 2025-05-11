20d265ac47d3498a96743959c1a94e9e96560247
yousuf-safin <yousufalisafin@gmail.com>
2025-05-12 01:57:36 +0600

    Operators

--- Var&Datatype/Operators.py
+++ Var&Datatype/Operators.py
@@ -10,3 +10,30 @@ b+=4
 print(b)
 a+=b
 print(a)
+
+#comparison operator
+d=4
+f=6
+g=d<f
+h = 4!=4
+print(h)
+print(g)
+
+#logical Operators
+
+k = True or False
+print(k) 
+#truth table of or
+print("True or False",True or True)
+print("True or False",True or False)
+print("True or False",False or True)
+print("True or False",False or False)
+
+#truth table of and
+print("True and False",True and True)
+print("True and False",True and False)
+print("True and False",False and True)
+print("True and False",False and False)
+
+print(not(True))
+print(not(False))
