18c4695134975ae072159693977179144aa65363
yousuf-safin <yousufalisafin@gmail.com>
2025-05-14 16:24:05 +0600

    Str Prac

--- String/escape.py
+++ String/escape.py
@@ -1,2 +1,3 @@
 a= "yousuf is a good boy \n but not a bad\t\"boy\""
 print(a)
+print(r"C:\Users\Yousuf")  # Output: C:\Users\Yousuf
---
+++ String/strpractice.py
@@ -0,0 +1,13 @@
+txt = " hello World"
+print(len(txt))
+print(txt.upper())
+print(txt.lower())
+print(txt.strip())#Remove leading and trailing whitespace
+print(txt.replace("H","P"))
+text="apple, banana, cherry"
+print(text.split(" "))
+words= ["apple","banana","cherry"]
+print(" ".join(words))
+
+print(txt.find("World"))
+print(txt.capitalize())
