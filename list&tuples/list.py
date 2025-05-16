b5367996731e97042d22793fface1e2d033bcb3c
yousuf-safin <yousufalisafin@gmail.com>
2025-05-16 23:49:35 +0600

    Python List & Tuple

--- list&tuples/list.py
+++ list&tuples/list.py
@@ -1,4 +1,7 @@
 friends = ["Yousuf", "Apple", 5, 22.25, False, "Yousha", "Ali"]
-print(friends[2])
-friends[0]="Mango"
 print(friends[0])
+friends[0]="Mango" #unlike strings lists are mutable
+print(friends[0])
+print(friends[5])
+print(friends[-1])
+print(friends[2:5])
