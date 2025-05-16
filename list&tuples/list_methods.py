456b2b062e6e29b6f20903e0881b20732da2e05e
yousuf-safin <yousufalisafin@gmail.com>
2025-05-16 23:57:37 +0600

    Python List & Tuple

--- list&tuples/list_methods.py
+++ list&tuples/list_methods.py
@@ -2,11 +2,13 @@ number = [5,7,9,3,11,22,10]
 print(number)
 number.sort()
 print(number)
-number.insert(2,4) #add 4 at 2nd index
+number.insert(2,98) #add 4 at 2nd index
 print(number)
 number.pop(5)#remove 5th index
 print(number)
-number.remove(4)#remove 4
+number.remove(98)#remove 4
+print(number)
+number.reverse()
 print(number)
 
 friends = ["Yousuf", "Apple", 5, 22.25, False, "Yousha", "Ali"]
