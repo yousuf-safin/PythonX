3d1bbe4f98a7ff2512e4936bb49b5f9c26fd3f64
yousuf-safin <yousufalisafin@gmail.com>
2025-05-24 23:28:07 +0600

    Revision

--- practice.py
+++ practice.py
@@ -5,5 +5,14 @@ print(b%a)
 
 
 
-z=int(input("Enter a Number :"))
-print(type(z))
+# z=int(input("Enter a Number :"))
+# print(type(z))
+
+# comparison
+print(a>b)
+
+x= int(input("Enter a Number :"))
+y= int(input("Enter a Number :"))
+r= x+y
+print("Avg is",r/2)
+print("Square of a number",x*x)
