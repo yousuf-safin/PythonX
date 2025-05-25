3328951327e621644e35da4e41a5022631cdd233
yousuf-safin <yousufalisafin@gmail.com>
2025-05-25 23:36:18 +0600

    TUple_Method

--- list&tuples/tuples.py
+++ list&tuples/tuples.py
@@ -1,2 +1,5 @@
 a=(1,2,3,4)
+b= (23,"Imu",34,909,"Yousuf")
+#b[0]=100#tupeles are immutable cant be changed
+print(b)
 print(type(a))
---
+++ list&tuples/tuples_method.py
@@ -0,0 +1,4 @@
+a=(23,"Imu",34,909,23,"Yousuf",23,909,909,"Yousuf")
+print(a)
+print(a.count(23)) #for counting how many times it appear in the tuple
+print(a.index("Yousuf"))
