480bde8e53cd0ba91090f2a2722c2d8a56b4d736
yousuf-safin <yousufalisafin@gmail.com>
2025-05-25 23:42:06 +0600

    TUple_Method

--- list&tuples/tuples_method.py
+++ list&tuples/tuples_method.py
@@ -1,4 +1,11 @@
 a=(23,"Imu",34,909,23,"Yousuf",23,909,909,"Yousuf")
+b=(1,2,3,4)
 print(a)
 print(a.count(23)) #for counting how many times it appear in the tuple
 print(a.index("Yousuf"))
+concat = b + a #concatened 2 tuples together
+print(concat)
+print("Yousuf" in a) #checking tuple is in the tuple
+print(len(a)) #length of the tuple
+print(max(b))#max value of the tuple
+print(min(b))
