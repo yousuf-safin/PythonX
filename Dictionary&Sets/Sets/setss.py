b98c214bf5672dc7cb2016144d70747433d49f7d
yousuf-safin <yousufalisafin@gmail.com>
2025-06-14 23:56:34 +0600

    Sets

--- Dictionary&Sets/Sets/setss.py
+++ Dictionary&Sets/Sets/setss.py
@@ -2,6 +2,10 @@ s={1,2,3,45,"Hamza"} #set
 print(s,type(s))
 s.add(69)
 print(s)
-s.remove("Hamza_")
+s.remove("Hamza")
 print(s)
 e= set(s) #empty set
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8}
+print(s1.union(s2))
+print(s1.intersection(s2))
