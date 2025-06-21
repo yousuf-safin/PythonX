810e9969a351d137a5bd9e61215dd025a2e777bb
yousuf-safin <yousufalisafin@gmail.com>
2025-06-21 23:35:06 +0600

    Conditionals

---
+++ Condition(if/else)/conditional.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your Age :"))
+
+if (a>18):
+    print("You can drive")
+elif(a<0):
+    print("You are entering into a negative age")
+
+else:
+    print("You can't drive")
+
+print("\n Have a nice day ahead!")
