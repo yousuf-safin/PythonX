f4486dd1251ff998b7ac8c4c7befb009992ba376
yousuf-safin <yousufalisafin@gmail.com>
2025-06-22 22:47:43 +0600

    Examples

---
+++ python_Examples/find_factorial.py
@@ -0,0 +1,11 @@
+fact=1
+num = int(input("Enter Number : "))
+
+if num<0:
+    print("Factorial does not exist for negative numbers")
+elif num==0:
+    print("Factorial of 0 is 1")
+else:
+    for i in range(1,num+1):
+        fact=fact*i
+    print("Factorial of",num ,"is",fact)
