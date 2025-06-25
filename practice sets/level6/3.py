99cc5e52ccfab362392fcc100c3ab2bd50459550
yousuf-safin <yousufalisafin@gmail.com>
2025-06-25 23:49:25 +0600

    Conditional_Problems

--- practice sets/level6/3.py
+++ practice sets/level6/3.py
@@ -5,3 +5,7 @@ p4= "Click this"
 
 message = input("Enter Your Comment :")
 
+if(p1 in message) or (p2 in message) or (p3 in message) or (p4 in message):
+    print("This comment is Spam")
+else:
+    print("This comment is Not Spam")
