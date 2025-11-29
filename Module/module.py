227611acc852a76f9cd4574f5f357edf20910788
yousuf-safin <yousufalisafin@gmail.com>
2025-11-29 21:34:45 +0600

    Py New

--- Module/module.py
+++ Module/module.py
@@ -1,3 +1,6 @@
 import pyjokes
 joke = pyjokes.get_joke()
 print(joke)
+
+# This code imports the pyjokes library, retrieves a random joke, and prints it to the console. 
+# PIP is the package manager for Python, used to install and manage software packages written in Python.
